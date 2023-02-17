d6a0f7f9721cb75419fce47a5c621975522b768e
Garth Henson <garth@guahanweb.com>
2023-02-17 15:41:50 -0500

    adding server startup logic

--- apps/workflow-api-shim/src/index.ts
+++ apps/workflow-api-shim/src/index.ts
@@ -1,4 +1,5 @@
 import { initialize as initializeLogger } from "./logger";
+import { createServer } from "./server";
 import config from './config';
 
 main();
@@ -8,7 +9,10 @@ async function main() {
     logger.info('logger initialized');
 
     try {
-        logger.info('running');
+        const app = createServer();
+
+        await app.listen(config.port);
+        logger.info('server is listening', { config });
     } catch (err: any) {
         logger.error(err.message, { error: err });
     }
